97120b2a31310490a117cbbb94a434b4b78596bb
NureSheikoAndrii <andrii.sheiko@nure.ua>
2025-12-24 20:07:50 +0200

    Delete SmartRoom Backend Update 1 directory

--- SmartRoom Backend Update 1/SmartRoomBackend_Upd1/Models/AutomationSettings.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SmartRoomBackend_Upd1
-{ 
-    public class AutomationSettings
-    {
-        public int Id { get; set; }
-        public double MaxTemperature { get; set; }
-        public int MinLightLevel { get; set; } 
-    }
-}
--- SmartRoom Backend Update 1/SmartRoomBackend_Upd1/Models/SystemLog.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SmartRoomBackend_Upd1.Models
-{
-    public class SystemLog
-    {
-        public int Id { get; set; }
-        public string Message { get; set; }
-        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-    }
-}

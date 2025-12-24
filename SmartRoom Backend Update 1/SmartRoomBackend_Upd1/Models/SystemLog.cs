0db67de971e24896129033f929a5d49958e8d115
NureSheikoAndrii <andrii.sheiko@nure.ua>
2025-12-24 20:20:06 +0200

    Add files via upload

---
+++ SmartRoom Backend Update 1/SmartRoomBackend_Upd1/Models/SystemLog.cs
@@ -0,0 +1,9 @@
+﻿namespace SmartRoomBackend_Upd1.Models
+{
+    public class SystemLog
+    {
+        public int Id { get; set; }
+        public string Message { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
